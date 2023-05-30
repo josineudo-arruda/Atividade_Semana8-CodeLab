b3633db29377c0fa2f3ed8359e88a11fb80e3107
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-30 13:46:29 -0300

    feat: add app-routing.module.app

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -12,6 +14,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   imports: [
     BrowserModule,
     MatToolbarModule,
+    AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
