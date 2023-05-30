28fe0f16cdbfd154c635d0be42aaf63f231bd3c5
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-30 15:03:01 -0300

    feat: add api service in kson

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-courses',
@@ -8,9 +9,7 @@ import {CoursesService} from "../services/courses.service";
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
-  courses: Course[] = [
-    {_id: '1', name: 'Taylor Swift', category: 'Country'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
   // courseService: CoursesService;
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Course} from "../model/course";
+import {first} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-
+  private readonly API = '/assets/courses.json';
   constructor(private httpClient: HttpClient) { }
 
   list() {
-    return [
-      {_id: '1', name: 'Taylor Swift', category: 'Country'}
-    ];
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first()
+    );
   }
 }
