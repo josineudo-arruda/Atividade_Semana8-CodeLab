2e7524d2c4bc95a3d4942af2026dc0d82dc6f52a
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-06-01 15:09:13 -0300

    feat: add spring connection

---
+++ crud-spring/src/main/java/com/josineudo/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.josineudo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public @ResponseBody String hello() {
+        return "Hello World!";
+    }
+}
